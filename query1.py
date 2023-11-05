d1fd66052d4a78d200cd9e0db784b102f1881d33
b-ivanov2000 <bozhhider.ivanov@gmail.com>
2023-11-05 10:04:44 +0200

    Query1 TO DO Dump in .pickle

---
+++ query1.py
@@ -0,0 +1,30 @@
+import pickle
+import csv
+
+def read_csv(path: str, delimiter: str):
+    """
+    Reads the csv file defined in path and gives it as dict of lists
+    the dict keys are the variable names and the lists contain the
+    observation per variable
+    """
+    output = {}
+
+    with open(path, newline='') as csvfile:
+        data_temp = csv.DictReader(csvfile, delimiter=delimiter)
+
+        for row in data_temp:
+                for fieldname in data_temp.fieldnames:  
+                    if fieldname not in output.keys():
+                        output[fieldname] = [row[fieldname]]
+                    else:
+                        output[fieldname].append(row[fieldname])
+
+    print("done reading the file!")
+    return output
+
+countries = read_csv("data/Country.csv", delimiter=",")
+
+max_length = max(len(item) for item in countries["name"])
+
+longest_elements = [item for item in countries["name"] if len(item) == max_length]
+print("TO DO: dump in pickle")
