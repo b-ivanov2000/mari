7ea7f7e9421038841e3ad586f299dffa80d31d93
b-ivanov2000 <bozhhider.ivanov@gmail.com>
2023-11-15 16:31:34 +0200

    update na teoriq

--- bozho.py
+++ bozho.py
@@ -6,6 +6,7 @@
 #c. pop() by default removes the last item of the list
 
 #Which of the following claims are true?
+#b. After the two Python statements first = [] and second = first, second is an alias of first
 #c. After the execution of the Python statement first = [], the statements second = [] and second = first will produce the same references in memory
 #e. After the statement new_list = my_list, new_list is a clone of my_list
 
