7fe039b26d741701fab57d5a6dd47e5087cb95b7
b-ivanov2000 <bozhhider.ivanov@gmail.com>
2023-11-29 19:07:06 +0200

    reschenie

--- bozho.py
+++ bozho.py
@@ -1,1 +1,28 @@
 #!!! Poluchih OK
+
+class Wallet:
+    def __init__(self):
+        self.credit = 0
+
+    def load(self, amount):
+        self.credit = self.credit + amount
+    
+    def get_credit(self):
+        return self.credit
+    
+    def purchase(self, price):
+        if price > self.credit:
+            return False
+        else:
+            self.credit = self.credit - price
+            return True
+        
+w = Wallet()
+# Let's load some money
+w.load(10.00)
+# We buy an item
+success = w.purchase(6.00)
+print(success)
+another_success = w.purchase(5)
+print(another_success)
+print(w.get_credit())
